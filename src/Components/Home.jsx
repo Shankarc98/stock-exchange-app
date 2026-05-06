4ff15acf20b18834838572bacaf9eee99799d65e
Shankar Narayan <shankarnarayan1555@gmail.com>
2026-05-06 16:01:16 +0200

    changed dependencies

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -163,7 +163,7 @@ function Home(props){
       <div className="header main-color">
         <img className="stock-logo" src="/images/stock.svg" alt="stock market logo"/>
         <h1 className="brand">Stock Market Simulation</h1> 
-        <h2 className="wallet">Your Wallet = {wallet.wal}</h2>
+        <h2 className="wallet">Your Wallet = {Number(wallet.wal.toFixed(2))}</h2>
       </div>
       <div className="navigation" >
         <Timer /> 
