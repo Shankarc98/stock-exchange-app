3db6e4345c7a10a061f4a6e477554784bf2f2eaa
Shankar Narayan <shankarnarayan1555@gmail.com>
2026-08-24 22:12:17 +0530

    add mobile card

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,19 +2,22 @@ import React from "react";
 
 function Card(props){
         
-    return <div className="card-cont">
-            <div className="logo-comp">
-                <img  src={props.picture} className="logo" alt="avionics logo"/>
-                <p className="comp-name">{props.name}</p>
-            </div> 
+ return <div className="card-cont">
+           <div className = "name-price">
+                <div className="logo-comp">
+                    <img  src={props.picture} className="logo" alt={props.name + "logo"}/>
+                    <p className="comp-name">{props.name}</p>
+                </div> 
 
             <p className="price">₹ {(props.price).toFixed(2)}</p>
             <p className="change" style={{color: props.direction == 1 ? "green" : props.direction == -1 ? "red" : "black"}}>₹ {props.direction == 1? "↑" : props.direction == -1 ?  "↓" : "—"} {(props.change).toFixed(2)}</p>
               
+           </div> 
+            
 
         <div className="card-buttons">
-            <button onClick={() => props.handleClick(1, props.name, props.price, props.id)} className="buy-btn btn" type="button">Buy</button>
-            <button onClick={() => props.handleClick(2, props.name, props.price, props.id)} className="sell-btn btn" type="button">Sell</button>
+            <button onClick={() => props.handleClick(1, props.name, props.price, props.id)} className="btn" type="button">Buy</button>
+            <button onClick={() => props.handleClick(2, props.name, props.price, props.id)} className="btn" type="button">Sell</button>
         </div>
     </div>
 }
--- src/Components/Transaction.jsx
+++ src/Components/Transaction.jsx
@@ -4,14 +4,15 @@ function Transaction(props){
     
     const [focus, setFocus] = useState(false); 
 
+    
     return <div style = {{backgroundColor : focus ? "aliceblue" : "white"}} key={props.id} className="history-record hist-row" onMouseEnter={() => {setFocus(true)}} onMouseLeave={() => setFocus(false)}>
          <p className="transactionType transactionRec">{props.type}</p>
          <p className="transactionName transactionRec">{props.compName}</p>
          <p className="transactionShares transactionRec">{props.sharesTraded}</p>
          <p className="transactionPrice transactionRec">{props.atPrice}</p>
          <p className="transactionMoney transactionRec">{props.money}</p>
-         <button className="del-transaction" onClick={() => props.deleteHistory(props.id)} style={{display : focus ? "block" : "none"}}>🗑</button>
-</div>
+         <button className="del-transaction" onClick={() => props.deleteHistory(props.id)} style={{display : focus ? "block" : "none"}} type="button">🗑</button>
+    </div>
 }
 
 export default Transaction;
